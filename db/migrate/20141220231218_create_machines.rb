2ce8e2256699669f0b07518eb9ff4c357c1a116f
AJ Foster <web@aj-foster.com>
2014-12-20 20:59:09 -0500

    Add machine model.

---
+++ db/migrate/20141220231218_create_machines.rb
@@ -0,0 +1,12 @@
+class CreateMachines < ActiveRecord::Migration
+	def change
+		create_table :machines do |t|
+			t.string :name
+			t.string :location
+			t.string :ip_address
+			t.hstore :info
+
+			t.timestamps
+		end
+	end
+end
