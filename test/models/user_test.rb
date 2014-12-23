58733536317824d6b86d04b86884e14688ef52db
AJ Foster <web@aj-foster.com>
2014-12-22 23:56:31 -0500

    Add part of attribute validation tests for users.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	# Attribute validation tests
+	
+	def test_name_presence_validation
+		user_no_name = users(:user_no_name)
+		assert_not user_no_name.save, "Saved user without a name."
+	end
 end
