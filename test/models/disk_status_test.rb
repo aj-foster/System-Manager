746e3f3b7c2fb19424af02be95a73843110f46fe
AJ Foster <web@aj-foster.com>
2014-12-22 22:33:57 -0500

    Add validation tests for disk statuses.

--- test/models/disk_status_test.rb
+++ test/models/disk_status_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class DiskStatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	# Attribute validation tests
+
+	def test_disk_presence_validation
+		disk_status_no_disk = disk_statuses(:disk_status_no_disk)
+		assert_not disk_status_no_disk.save, "Saved disk status without a disk."
+	end
 end
