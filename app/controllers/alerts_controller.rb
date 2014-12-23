8e999fcf268809143fa97a12e0ca73b148104321
AJ Foster <web@aj-foster.com>
2014-12-23 16:05:22 -0500

    Add controller actions and preliminary views for alerts.

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,11 +1,20 @@
 class AlertsController < ApplicationController
 
 	def index
+		@alerts = Alert.order(updated_at: :desc)
 	end
 
 	def show
+		@alert = Alert.find(params[:id])
 	end
 
 	def destroy
+		@alert = Alert.find(params[:id])
+
+		if @alert.destroy
+			redirect_to alerts_path, notice: "The alert, #{@alert.name}, has been removed."
+		else
+			redirect_to @alert, alert: "Error: Could not remove the alert."
+		end
 	end
 end
