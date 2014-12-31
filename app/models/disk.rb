4e97db32ab50eccbedc314f09fbecbdab4d69e02
AJ Foster <web@aj-foster.com>
2014-12-30 21:53:50 -0500

    Add tile structure to disks index, and add two disk methods: one for getting a nice readout of the disk's capacity, and the other for its location.

--- app/models/disk.rb
+++ app/models/disk.rb
@@ -9,5 +9,26 @@ class Disk < ActiveRecord::Base
 
 	validates :name, :serial_number, :capacity, :format, presence: true
 	validates :name, uniqueness: { case_sensitive: false }
-	
+
+	# Methods
+
+	def nice_capacity
+		if capacity.blank?
+			"Unknown size"
+		elsif capacity < 1000
+			"#{capacity}GB"
+		else
+			"#{capacity / 1000.0}TB"
+		end
+	end
+
+	def location
+		if machine.blank?
+			""
+		elsif usage.blank?
+			"#{machine.name}"
+		else
+			"#{machine.name} (#{usage})"
+		end
+	end
 end
