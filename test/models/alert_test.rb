07f8aa3f7920c1b3e63f3e0b45041d2375a17dd5
AJ Foster <web@aj-foster.com>
2015-01-31 02:27:03 -0500

    Add tests for Alert.touch.

--- test/models/alert_test.rb
+++ test/models/alert_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class AlertTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	# Tests for Alert::touch
+	#
+
+	def test_nil_attribute_rejection
+		assert_not Alert.touch, "Accepted Alert.touch without attributes."
+	end
+
+	def test_existing_alert_touch
+		alert_id = alerts(:alert_canonical).id
+
+		assert_difference 'Alert.find(alert_id).multiplicity', 1, "Didn't increment multiplicity of existing alert" do
+			Alert.touch(
+				name: "Canonical Name",
+				message: "Alert Message",
+				alertable: disks(:disk_canonical)
+			)
+		end
+	end
+
+	def test_new_alert_touch
+		assert_difference 'Alert.count', 1, "Didn't create new Alert" do
+			Alert.touch(
+				name: "New Alert",
+				message: "Foreign message",
+				alertable: disks(:disk_canonical)
+			)
+		end
+	end
 end
