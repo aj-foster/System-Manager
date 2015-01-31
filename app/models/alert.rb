2a4dd0affcfa86f22aa9429ee4ecfc074ec50c15
AJ Foster <web@aj-foster.com>
2015-01-31 01:58:18 -0500

    Cleanup and document alert model.

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,18 +1,45 @@
+# Alerts give the user warnings about alertable objects (i.e. disks and
+# machines) based on data collected by the application. These are displayed
+# immediately upon login and they attach themselves to relevant objects.
+#
+# ==== Fields
+#
+# * +name+ - A short, reusable name for the alert which summarizes its
+#   meaning for an experienced user.
+# * +message+ - A longer, more detailed description of the alert.
+# * +multiplicity+ - The number of times this alert has been noted.
+# * +alertable+ - An optional reference to the object which the alert
+#   concerns.
+#
 class Alert < ActiveRecord::Base
 
-	# Relationship
+	# Relationships
+	#
 	belongs_to :alertable, polymorphic: true
 
 
-	# Default Setup
+	# Callbacks
+	#
+
+	# Give alerts safe default attributes.
 	before_save :setup_alert
 
 
-	# Allows an alert to be created if it doesn't exist or updated with a "+1"
-	# if it does.
+	# Allows an alert to be created if it doesn't exist or given +1
+	# multiplicity if it does.
 	#
+	# ==== Parameters
+	#
+	# * +attrs+ - Attributes of the Alert to touch. These will be passed into
+	#   a +where+ query. *Note*: Insufficient attributes supplied by the caller
+	#   could cause an increase in multiplicity to many alerts.
+	#
+	# ==== Example
+	#
+	#   Alert.touch(name: "Reallocations", message: "...", alertable: @disk)
+	#--
 	# TODO: make arguments to this method match up with attributes of Alerts.
-
+	#
 	def self.touch (attrs = nil)
 
 		# Avoid adding +1 to every alert.
@@ -38,6 +65,8 @@ class Alert < ActiveRecord::Base
 
 	private
 
+		# Gives a new alert a multiplicity of 1 by default.
+		#
 		def setup_alert
 			self.multiplicity ||= 1
 		end
