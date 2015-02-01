a2b6cc89d5c51a169c91eec36dacc4d7d7c7a331
AJ Foster <web@aj-foster.com>
2015-01-31 23:07:24 -0500

    Add form for creating new machine extended attributes.

--- app/controllers/extended_attributes_controller.rb
+++ app/controllers/extended_attributes_controller.rb
@@ -1,2 +1,21 @@
 class ExtendedAttributesController < ApplicationController
+
+	before_filter :authenticate_user!
+
+	def create
+		@ex_attr = ExtendedAttribute.create(ex_attr_params)
+
+		if @ex_attr.save
+			redirect_to :back, notice: "The new attribute, #{@ex_attr.name}, has been saved."
+		else
+			redirect_to :back, alert: "The new attribute could not be saved."
+		end
+	end
+
+
+	private
+
+		def ex_attr_params
+			params.require(:extended_attribute).permit!
+		end
 end
